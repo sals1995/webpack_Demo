e8472c4acc4408c0f7be478fd616f43ca9e82f32
Salsabeel Hussien <58127260+sals1995@users.noreply.github.com>
2022-03-13 13:52:37 +0200

    images optimization plugins updated

--- src/index.js
+++ src/index.js
@@ -1,9 +1,51 @@
 import hello from './hello world';
 hello()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 import header from "./component/header/header"
-// // nooo
+// // // nooo
+
+
+
+
 import "./style.css"
+
+
+
+
+
+
 import bio from "./component/bio/bio"
+import photo from"./assets/cover.jpg"
+
+
 /* 
 this is a big comment
  */
--- webpack.config.js
+++ webpack.config.js
@@ -1,65 +1,76 @@
-const pathModule = require("path")
-const htmlPlugin = require("html-webpack-plugin")
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
-const ImageMinimizerPlugin = require('image-minimizer-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const pathModule = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ImageMinimizerPlugin = require("image-minimizer-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
 module.exports = {
-  mode: "production",
-  entry: "./src/index.js",
-  output: {
-    filename: "bundle.min.js"
-    , path: pathModule.resolve(__dirname, "dist"),
-    assetModuleFilename: 'images/[name][ext]'
-  },
-  module: {
-    rules: [
-      {
-        test: /\.css$/i,
-        use:  [MiniCssExtractPlugin.loader, "css-loader"],
-      },
-      {
-        test: /\.s[ac]ss$/i,
-        use: [
-          // Creates `style` nodes from JS strings
-          MiniCssExtractPlugin.loader,
-          // Translates CSS into CommonJS
-          "css-loader",
-          // Compiles Sass to CSS
-          "sass-loader",
-        ],
-      },
-      {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: 'asset/resource',
-      },
-    ]
-  }
-  , plugins: [
-    new htmlPlugin({template:"src/index.html"}),
-    new MiniCssExtractPlugin({ filename: "styles.css" }),    
-    new CleanWebpackPlugin(),
-    new CssMinimizerPlugin()
-      
-  ],
-  optimization:{
-    minimize:true,
-    minimizer:[
-      
-      "...",
-      new ImageMinimizerPlugin({
-        minimizerOptions: {
-            // Lossless and lossy optimization with custom option
-            // Feel free to experiment with options for better result for you
-            plugins: [
-              ["gifsicle", { interlaced: true }],
-              ["mozjpeg",{quality:70}],
-              ["optipng", { optimizationLevel: 5 }],
-              // Svgo configuration here https://github.com/svg/svgo#configuration
-             ['svgo']
+    mode:"production",
+    entry:"./src/index.js",
+    output:{
+        filename:"bundle.js",
+        path:pathModule.resolve(__dirname,"build"),
+        assetModuleFilename: 'images/[name][ext]'
+    },
+    module:{
+        rules: [
+            //css files
+            {
+              test: /\.css$/i,
+              use: [MiniCssExtractPlugin.loader, "css-loader"],
+            },
+            //images
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+              },
+              {
+                test: /\.s[ac]ss$/i,
+                use: [
+                  // Creates `style` nodes from JS strings
+                  MiniCssExtractPlugin.loader, "css-loader", "sass-loader",
+                ],
+              },
+          ],
+    },
+    plugins:[new HtmlWebpackPlugin(),new MiniCssExtractPlugin(),
+      new CssMinimizerPlugin()
+    ],
+          optimization: {
+            minimizer: [
+                //terser
+              "...",
+              new ImageMinimizerPlugin({
+                minimizer: {
+                  implementation: ImageMinimizerPlugin.imageminMinify,
+                  options: {
+                    // Lossless optimization with custom option
+                    // Feel free to experiment with options for better result for you
+                    plugins: [
+                      ["gifsicle", { interlaced: true }],
+                      [ "mozjpeg",{ quality: 60}],
+                      ["optipng", { optimizationLevel: 5 }],
+                      // Svgo configuration here https://github.com/svg/svgo#configuration
+                      [
+                        "svgo",
+                        {
+                              name: 'preset-default',
+                              params: {
+                                overrides: {
+                                  // customize plugin options
+                                  convertShapeToPath: {
+                                    convertArcs: true
+                                  },
+                                  // disable plugins
+                                  convertPathData: false
+                                }
+                              }
+                        }
+                      ],
+                    ],
+                  },
+                },
+              }),
             ],
-        },
-      }),
-    ]
-  }
+          }
+       
 }
